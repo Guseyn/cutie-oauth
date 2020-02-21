690d78a1206f64c676912385e93cbbb74168d4bf
guseyn <guseynism@gmail.com>
2020-02-21 15:24:36 +0300

    resolve #9

--- src/GitHubAuthRequestOptions.js
+++ src/GitHubAuthRequestOptions.js
@@ -4,16 +4,16 @@
 const { AsyncObject } = require('@cuties/cutie')
 
 class GitHubAuthRequestOptions extends AsyncObject {
-  constructor (code) {
+  constructor (code, clientId, clientSecret) {
     super(code)
   }
 
   syncCall () {
-    return (code) => {
+    return (code, clientId, clientSecret) => {
       return {
         hostname: 'github.com',
         port: 443,
-        path: `/login/oauth/access_token?client_id=9740bb12713949b1c23d&client_secret=300c8427a2562a2851e4dc7dbc1e3a7b50328c8c&code=${code}`,
+        path: `/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}`,
         method: 'POST',
         headers: {
           'Accept': 'application/json'
